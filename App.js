42d1555008a805a6e55ee701f3dc32e260c50ee7
rkeerthi <sunnykeerthi@gmail.com>
2022-11-24 18:30:11 +0530

    updated safearea view

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
-    <View>
-      
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flexGrow: 0,
+          flexShrink: 1,
+          backgroundColor: "green",
+          padding: 16,
+        }}
+      >
+        <Text>Search</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexGrow: 1,
+          backgroundColor: "blue",
+          padding: 16,
+        }}
+      >
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+});
